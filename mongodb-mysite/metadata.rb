34497f44ac3286e0bd34558b3692837e30e17f26
Pramod Singh <pramod.singh@kiwitech.com>
2016-12-09 10:44:35 +0530

    added mongodb recipes

---
+++ mongodb-mysite/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-mysite"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "KiwiTech"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
